3611999fa7d6f4c591df8a7517de2a498355b8dc
ArtemZdanovich <gilyen2@mail.ru>
2022-09-25 22:29:03 +0300

    задача четные/нечетные

--- lesson2/src/test2.java
+++ lesson2/src/test2.java
@@ -1,35 +1,24 @@
+/**
+ * Простая (2 балла)
+ *
+ * Определить, четное ли данное число
+ *
+ */
 import java.util.Scanner;
 public class test2 {
-    public static void main(String[] args) {
+    public static void main (String[] args) {
         Scanner in = new Scanner(System.in);
-
-
-        System.out.print("Введите часы:\n");
-        int hh = in.nextInt();
-        if (hh > 24 | hh < 0) {
-            System.out.print("ввод некорректен");
-
-        } else {
-
-
-            System.out.print("введите минуты\n");
-        }
-        int mm = in.nextInt();
-        if (mm > 60 | mm < 0) {
-            System.out.print("ввод некорректен");
-        } else {
-            System.out.print("Введите секунды\n");
-
-        }
-        int ss = in.nextInt();
-        if (ss>60|ss<0) {
-            System.out.println("ввод некорректен");
+        System.out.println("Введите число:");
+        int num = in.nextInt();
+        if ( num%2!=0 ) {
+            System.out.printf ("Вы ввели нечетное число %d.", num);
         }
         else {
-            int x=hh*60*60+mm*60+ss;
-            System.out.println ("истекло секунд: " + x);
+            System.out.printf ("Вы ввели четное число %d.", num);
         }
-        in.close ();
+
+        in.close();
+
     }
 }
 
