f304541b41b03c4e00e7e7675967805bc3926f55
ArtemZdanovich <gilyen2@mail.ru>
2022-09-26 01:16:28 +0300

    Task 3

--- lesson2/src/test2.java
+++ lesson2/src/test2.java
@@ -1,26 +1,21 @@
 /**
  * Простая (2 балла)
- *
- * Определить, четное ли данное число
- *
+ * Пользователь задает целое трехзначное число (например, 478).
+ * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
 import java.util.Scanner;
 public class test2 {
     public static void main (String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите число:");
-        int num = in.nextInt();
-        if ( num%2!=0 ) {
-            System.out.printf ("Вы ввели нечетное число %d.", num);
-        }
-        else {
-            System.out.printf ("Вы ввели четное число %d.", num);
-        }
-
-        in.close();
+        System.out.print("Введите целое трехзначное число:\n");
+        int x = in.nextInt();
+        int a = x / 100;
+        int ax = x % 100;
+        int b = ax / 10;
+        int c = ax % 10;
+        int y = c * 100 + b * 10 + a;
+        System.out.println(y);
+                in.close();
 
     }
 }
-
-
-//  in.close ();
